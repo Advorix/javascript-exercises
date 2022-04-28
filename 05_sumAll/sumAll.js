fb34ee6dd1ce0d9b0d04454a3973c3af6f429dd7
Advorix <whatever1692@gmail.com>
2022-04-28 07:16:42 -0500

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, range) {
+    if (start > range) {
+        let temp = range;
+        range = start;
+        start = temp;
+    }
+    if (start < 0 || !Number.isInteger(start) || !Number.isInteger(range)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    for (let i = start; i <= range; i++) {
+        sum+= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
