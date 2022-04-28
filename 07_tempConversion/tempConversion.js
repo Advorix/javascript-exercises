9f9b87facf9d5383f96bc84d76641bb72e5934d0
Advorix <whatever1692@gmail.com>
2022-04-28 07:16:16 -0500

    Complete exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    return Math.round(((fahrenheit - 32) * 5/9) * 10.0) /10.0
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    return Math.round((celsius * 9/5 + 32) * 10.0) / 10.0
 };
 
 // Do not edit below this line
